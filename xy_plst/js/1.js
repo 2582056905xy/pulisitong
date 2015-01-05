ece6be29093c638154f9e1529d9bf7a355227d31
2582056905yuyu <2582056905@qq.com>
2015-01-05 21:49:01 +0800

    commit__0

---
+++ xy_plst/js/1.js
@@ -0,0 +1,27 @@
+//Tab切换
+window.onload=function(){
+	 oLis=document.getElementById('cont_nav').getElementsByTagName('li');
+	 oDiv=document.getElementById('cont_infor').getElementsByTagName('div');
+	if(oLis.length!=oDiv.length)
+    { return;}
+	for(var i=0;i<oLis.length;i++)
+	{
+		oLis[i].id=i;
+		oLis[i].onmouseover=function(){
+			for(var j=0;j<oLis.length;j++)
+			{
+				oLis[j].className=' ';
+				oDiv[j].style.display='none';
+			}
+			this.className='nowstate';
+			oDiv[this.id].style.display='block';
+		};
+	}
+};
+
+
+	
+	
+
+	
+	
